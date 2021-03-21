35fc98e8ca18c99137c6b294179767e4108529cb
coderboi <coderboi@gmail.com>
2021-03-21 08:15:27 +0000

    add pre commit test

---
+++ deployment/pre_commit.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env bash
+
+echo "Running pre-commit hook"
+dir=$(pwd)
+#$dir/unittest/run_tests.sh
+node $dir/test/randomItem.test.js
+
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Tests must pass before commit!"
+ exit 1
+fi
